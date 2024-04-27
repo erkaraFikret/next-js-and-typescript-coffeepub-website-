83bc5116be56e983244c98896c8d1b7937c0006b
Fikret Erkara <fikreterkara_26@hotmail.com>
2024-04-27 13:19:43 +0300

    Project Completed

---
+++ app/page.tsx
@@ -0,0 +1,39 @@
+"use client"
+import Navbar from '@/components/Navbar/Navbar'
+import React, { useEffect } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+import Hero from '@/components/Hero/Hero';
+import Services from '@/components/Services/Services';
+import Banner from '@/components/Banner/Banner';
+import PlayStore from '@/components/PlayStore/PlayStore';
+import Testimonials from '@/components/Testimonials/Testimonials';
+import Footer from '@/components/Footer/Footer';
+
+
+const Home = () => {
+
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+  }, [])
+
+
+  return (
+    <>
+      <Navbar/>
+    <Hero/>
+    <Services/>
+    <Banner/>
+    <PlayStore/>
+    <Testimonials/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
